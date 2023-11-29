aff1a41ab75f0783ad040cb654f9678308e7a95b
pratwib <pratamawibi24@gmail.com>
2023-11-29 22:08:20 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
+import { HiHome } from "react-icons/hi";
+import { MdGroup } from "react-icons/md";
+
+import "./App.css";
+import Movie from "./pages/Movie";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <p id="titleGroup">Kelompok 08</p>
+      </header>
+      <Switch>
+        <Route path="/movie">
+          <Movie />
+        </Route>
+        <Route path="/movies">
+          <Profile />
+        </Route>
+      </Switch>
+      <footer>
+        <NavLink to="/movie" className="iconWrapper">
+          <HiHome className="icon" />
+          Movie
+        </NavLink>
+        <NavLink to="/profile" className="iconWrapper">
+          <MdGroup className="icon" />
+          Profile
+        </NavLink>
+      </footer>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Profile.js
@@ -0,0 +1,9 @@
+export default function Profile() {
+
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1 style={{ color: "white" }}>Profile</h1>
+      </div>
+    );
+  }
+  
